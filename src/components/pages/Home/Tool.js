ae7aa3a4b2daefb8edbb5a8e191c010434c63845
smfahim25 <smfahimhossen@gmail.com>
2022-05-23 22:53:49 +0600

    tools added

--- src/components/pages/Home/Tool.js
+++ src/components/pages/Home/Tool.js
@@ -5,7 +5,18 @@ const Tool = (props) => {
     // console.log(props.tool);
     const { img, name, description, price, quantity } = props.tool;
     return (
-        
+        <div class="card w-96 bg-base-100 shadow-lg">
+            <figure><img src={img} alt="Shoes" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">{name}</h2>
+                <p>{description.slice(0, 250)}</p>
+                <h3><span className='font-bold'> Price:</span> {price}</h3>
+                <h3><span className='font-bold'> Quantity:</span>  {quantity}</h3>
+                <div class="card-actions justify-end">
+                    <Link to='/purchase' class="btn btn-primary">Buy Now</Link>
+                </div>
+            </div>
+        </div>
     );
 };
 
