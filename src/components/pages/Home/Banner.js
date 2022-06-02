4a318fd603271f888a4dded0dcf916d968af9918
smfahim25 <smfahimhossen@gmail.com>
2022-06-02 19:49:01 +0600

    code changed

--- src/components/pages/Home/Banner.js
+++ src/components/pages/Home/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
             <div className="hero-content text-center text-neutral-content" >
                 <div className="max-w-md" >
                     <h1 className="mb-5 text-5xl font-bold" > Cars Costume</h1 >
-                    <p className="mb-5" > Here you will get best product and lowest rate. You can trust us.</p >
+                    <p className="mb-5 font-bold" > Here you will get best product and lowest rate. You can trust us.</p >
                     <button className="btn btn-primary" > Get Started</button >
                 </div >
             </div >
